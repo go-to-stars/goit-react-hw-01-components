14d82cdcbdbe8d748b992bec8d3fc97e986e3db7
go-to-stars <ivanstep@ukr.net>
2023-06-30 19:08:04 +0300

    added component Profile

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Profile } from './Profile/Profile.jsx';
+import user from './Profile/user.json';
+
+function App() {
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
+  );
+}
+
+export default App;
